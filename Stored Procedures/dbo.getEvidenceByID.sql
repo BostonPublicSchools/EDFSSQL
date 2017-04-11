5243450174bd0646022d9c59edebd2e4acf16647
Bryce Avery <bavery@bostonpublicschools.org>
2017-04-11 16:17:28 -0400

    More clean up

--- Stored Procedures/dbo.getEvidenceByID.sql
+++ Stored Procedures/dbo.getEvidenceByID.sql
@@ -7,29 +7,29 @@ GO
 -- Create date: 07/10/2012
 -- Description:	Get  Evidence by EvidenceID
 -- =============================================
-CREATE PROCEDURE [dbo].[getEvidenceByID]
-	@EvidenceID AS nchar(6) 
-
+CREATE PROCEDURE [dbo].[getEvidenceByID] @EvidenceID AS NCHAR(6)
 AS
-BEGIN
-	SET NOCOUNT ON;
-	SELECT  e.EvidenceID
-			,e.[FileName]
-			,e.FileExt
-			,e.FileSize
-			,e.[Description]
-			,e.Rationale
-			,e.CreatedByID
-			,em.NameLast + ', ' + em.NameFirst + ' ' + ISNULL(em.NameMiddle, '') + ' (' + em.EmplID + ')' AS CreatedBy
-			,CONVERT(varchar, e.CreatedByDt, 101) as CreatedByDt
-			,e.IsEvidenceViewed
-			,e.EvidenceViewedDt
-			,e.EvidenceViewedBy
-	FROM Evidence e
-	LEFT OUTER JOIN EmplPlanEvidence epv on epv.EvidenceID = e.EvidenceID
-	JOIN Empl em (NOLOCK) on em.EmplID = e.CreatedByID
-	WHERE e.IsDeleted = 0
-	AND e.EvidenceID = @EvidenceID
-END
+    BEGIN
+        SET NOCOUNT ON;
+        SELECT  e.EvidenceID ,
+                e.FileName ,
+                e.FileExt ,
+                e.FileSize ,
+                e.Description ,
+                e.Rationale ,
+                e.CreatedByID ,
+                em.NameLast + ', ' + em.NameFirst + ' ' + ISNULL(em.NameMiddle,
+                                                              '') + ' ('
+                + em.EmplID + ')' AS CreatedBy ,
+                CONVERT(VARCHAR, e.CreatedByDt, 101) AS CreatedByDt ,
+                e.IsEvidenceViewed ,
+                e.EvidenceViewedDt ,
+                e.EvidenceViewedBy
+        FROM    dbo.Evidence e ( NOLOCK )
+                LEFT OUTER JOIN dbo.EmplPlanEvidence epv ( NOLOCK ) ON epv.EvidenceID = e.EvidenceID
+                JOIN dbo.Empl em ( NOLOCK ) ON em.EmplID = e.CreatedByID
+        WHERE   e.IsDeleted = 0
+                AND e.EvidenceID = @EvidenceID;
+    END;
 
 GO
